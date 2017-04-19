ed46c00cce93d117370803d2fc86109510aeb834
hex <hex@hex.com>
2017-04-19 11:39:57 +0800

    add the EL and FH .xml in Robot_demonXII

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hex/Desktop/Robots
+# Build directory: /home/hex/Desktop/Robots/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(test_simple_walk "/home/hex/Desktop/Robots/build/bin/test_simple_walk")
